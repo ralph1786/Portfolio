2f1f1690f116c0b0120a29f864d15d224fb7109a
ralph1786 <ralph1786@gmail.com>
2020-04-01 23:23:18 -0400

    added comps for app structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
-import "./App.css";
+import React, { Fragment } from "react";
+import "./App.scss";
+import Contact from "./containers/Contact";
+import Home from "./containers/Home";
+import Projects from "./containers/Projects";
+import About from "./containers/About";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Fragment>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </Fragment>
+  );
 }
 
 export default App;
