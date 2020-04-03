0535aeb20f5dde969cbb3ad73e440a2e8120e302
ralph1786 <ralph1786@gmail.com>
2020-04-03 00:03:57 -0400

    created skills array

---
+++ src/data/skills.js
@@ -0,0 +1,27 @@
+const skills = [
+  { label: "HTML5", value: 9, color: "#d65311" },
+  { label: "CSS3", value: 9, color: "#3ea9fa" },
+  { label: "SASS", value: 8, color: "#f78fe1" },
+  { label: "JAVASCRIPT", value: 10, color: "#edd928" },
+  { label: "TYPESCRIPT", value: 6, color: "#0f4699" },
+  { label: "REACT", value: 10, color: "#61dafb" },
+  { label: "REDUX", value: 8, color: "#7825e6" },
+  { label: "D3", value: 4, color: "#ed7607" },
+  { label: "REACT-NATIVE", value: 8 },
+  { label: "NODE", value: 5, color: "#026e00" },
+  { label: "EXPRESS", value: 4 },
+  { label: "GRAPHQL", value: 3, color: "#f00aab" },
+  { label: "APOLLO", value: 5, color: "black" },
+  { label: "RUBY", value: 5, color: "#c90a0a" },
+  { label: "RUBY ON RAILS", value: 7, color: "#a81e0c" },
+  { label: "WEBPACK", value: 4, color: "#3cb7fa" },
+  { label: "GIT", value: 5, color: "#f14e32" },
+  { label: "MONGOOSE", value: 4 },
+  { label: "MONGODB", value: 4, color: "#069906" },
+  { label: "SQL", value: 3 },
+  { label: "JEST", value: 3, color: "#992637" },
+  { label: "ENZYME", value: 3 },
+  { label: "PYTHON(pending)", value: 8, color: "#3776ab" }
+];
+
+export default skills;
