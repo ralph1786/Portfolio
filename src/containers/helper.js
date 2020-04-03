9d376b081bc3fb2d0b23f9ef1b8e02335d81dd67
ralph1786 <ralph1786@gmail.com>
2020-04-03 00:03:36 -0400

    fixed bug with time

--- src/containers/helper.js
+++ src/containers/helper.js
@@ -1,11 +1,11 @@
 import Evening from "../assets/eveningtown.jpg";
-import Morning from "../assets/morningcoffee.jpg";
+import Morning from "../assets/morningtree.jpg";
 import Afternoon from "../assets/afternoonsunset.jpg";
 
 export const message = time => {
   if (time > 6 && time < 12) {
     return "Good Morning";
-  } else if (time > 12 && time < 18) {
+  } else if (time >= 12 && time <= 18) {
     return "Good Afternoon";
   } else {
     return "Good Evening";
@@ -15,7 +15,7 @@ export const message = time => {
 export const backgroundImage = time => {
   if (time > 6 && time < 12) {
     return Morning;
-  } else if (time > 12 && time < 18) {
+  } else if (time >= 12 && time <= 18) {
     return Afternoon;
   } else {
     return Evening;
