b817234f45ac44219ce8babe7293e42345f63e68
ralph1786 <ralph1786@gmail.com>
2020-04-03 23:35:27 -0400

    created file with project data

---
+++ src/data/projects.js
@@ -0,0 +1,87 @@
+export const PROJECTS = [
+  {
+    name: "DaycareManager",
+    demo: "https://youtu.be/k-yE_-2nhCA",
+    github: "https://github.com/ralph1786/clientMngrFrontEnd",
+    description:
+      "Application for daycare that facilitates the management of its children's information. In addition it allows the parent to review its child's latest information and make payments to the daycare.",
+    frontendTech: [
+      "React",
+      "Sass",
+      "Redux",
+      "React Router",
+      "Redux-Thunk",
+      "Axios"
+    ],
+    testingTech: ["Jest", "Enzyme", "Sinon"],
+    backendTech: ["Ruby on Rails", "Bcrypt", "JWT", "Active Record"],
+    database: "PostgreSQL"
+  },
+  {
+    name: "FanChat",
+    demo: "https://youtu.be/k5_HTJ_TILo",
+    github: "https://github.com/ralph1786/FrontEndMod4Project/tree/rafael2",
+    description:
+      "Application for NBA fans where they are able to access different chat-rooms depending on their favorite team. They will also receive the latest sport news. While fans are engaging each other, they can take a look at the day's game scores. Fans are able to click at an specific game and purchase tickets for that game.",
+    frontendTech: [
+      "React",
+      "CSS3",
+      "React Router",
+      "Moment",
+      "Axios",
+      "React Spinner",
+      "React-ActionCable",
+      "@emotion/core"
+    ],
+    backendTech: ["Ruby on Rails", "Action Cable", "Active Record"],
+    database: "PostgreSQL"
+  },
+  {
+    name: "CoolSpots",
+    demo: "https://youtu.be/HiOLW9ly8rc",
+    github: "https://github.com/ralph1786/coolSpotFrontEnd/tree/master",
+    description:
+      "Application where users can share exciting places they have visited around the world. They can add reviews to these places as well.",
+    frontendTech: ["HTML5", "CSS3", "JavaScript", "Anime.js"],
+    backendTech: ["Ruby on Rails", "Active Record"],
+    database: "PostgreSQL"
+  },
+  {
+    name: "CryptoDash",
+    demo: "https://youtu.be/SGnCzh3D3qY",
+    github: "https://github.com/ralph1786/CryptoDash",
+    description:
+      "Application for individuals interested in keeping track of the current value of the top cryptocurrencies on the market. It provides and displays historical price of those cryptocurrencies(last 30 days). Information is obtained through the use of two external APIs (nomics API, cryptocompare API).",
+    frontendTech: [
+      "React",
+      "Sass",
+      "Redux",
+      "Redux-Thunk",
+      "Redux-Persist",
+      "React Router",
+      "Recharts",
+      "Three.js",
+      "GraphQL",
+      "Apollo",
+      "Axios"
+    ],
+    backendTech: ["Express", "GraphQL", "Axios", "Express-GraphQL"]
+  },
+  {
+    name: "CryptoDashMobile",
+    demo: "https://youtu.be/aEpPt7dqIUg",
+    github: "https://github.com/ralph1786/cryptoDashMobile",
+    description:
+      "Mobile Application for individuals interested in keeping tracking of the current value of the top crypto currencies on the market. It provides and displays historical price of those crypto currencies (last 10 days). Information is obtained through the use of two external APIs(nomics API, cryptocompare API)",
+    frontendTech: [
+      "React-Native",
+      "Redux",
+      "React-Native Navigation",
+      "Axios",
+      "React-Native-SVG-Chart",
+      "GraphQL",
+      "Apollo"
+    ],
+    backendTech: ["Express", "GraphQL", "Axios", "Express-GraphQL"]
+  }
+];
