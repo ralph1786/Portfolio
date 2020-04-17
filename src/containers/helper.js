c6b16a22e660ce1cd59cd9ff95f7ef9137b215ac
ralph1786 <ralph1786@gmail.com>
2020-04-16 23:08:46 -0400

    updated import path

--- src/containers/helper.js
+++ src/containers/helper.js
@@ -1,8 +1,8 @@
-import Evening from "../assets/eveningtown.jpg";
-import Morning from "../assets/morningtree.jpg";
-import Afternoon from "../assets/afternoonsunset.jpg";
+import Evening from "../assets/images/eveningtown.jpg";
+import Morning from "../assets/images/morningtree.jpg";
+import Afternoon from "../assets/images/afternoonsunset.jpg";
 
-export const message = time => {
+export const message = (time) => {
   if (time > 6 && time < 12) {
     return "Good Morning";
   } else if (time >= 12 && time <= 18) {
@@ -12,7 +12,7 @@ export const message = time => {
   }
 };
 
-export const backgroundImage = time => {
+export const backgroundImage = (time) => {
   if (time > 6 && time < 12) {
     return Morning;
   } else if (time >= 12 && time <= 18) {
