a79227c2ecd9c92d298b3cd3656ec32be2500fb6
ralph1786 <ralph1786@gmail.com>
2020-04-18 13:02:23 -0400

    added modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./App.scss";
 import Contact from "./containers/Contact";
 import Home from "./containers/Home";
 import Projects from "./containers/Projects";
 import About from "./containers/About";
+import ModalContent from "./components/ModalContent";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "40%",
+  },
+};
+
+Modal.setAppElement("#root");
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [chosenProject, setChosenProject] = useState(null);
+
+  const closeModal = () => setIsModalOpen(false);
+  const openModal = () => setIsModalOpen(true);
+  const selectedProject = (project) => setChosenProject(project);
+
   return (
     <Fragment>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        overlayClassName="Overlay"
+      >
+        <ModalContent project={chosenProject} closeModal={closeModal} />
+      </Modal>
       <Home />
       <About />
-      <Projects />
+      <Projects openModal={openModal} selectedProject={selectedProject} />
       <Contact />
     </Fragment>
   );
 }
 
-export default App;
+export default React.memo(App);
